98b390f65d63f3a502d841021bfe3c94dd67ecdd
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-04 23:12:13 +0530

    code updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
-require('dotenv').config()
+const bodyParser = require('body-parser')
+
+const { PORT } = require('./config/serverConfig')
 
 const setupAndStartserver = async () => {
     const app = express();
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
+
     app.listen(3000, () => {
         console.log(`Server started at ${process.env.PORT}`)
     })
