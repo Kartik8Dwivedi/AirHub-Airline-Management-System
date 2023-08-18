f99fb7bb1a15ce5f1533f2b6efd35bd3e01015bd
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-18 17:06:43 +0530

    db setup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const { PORT } = require('./config/serverConfig')
+const {City} = require('./models/index')
 
 const setupAndStartserver = async () => {
     const app = express();
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended:true}))
 
-    app.listen(3000, () => {
+    app.listen(3000, async () => {
         console.log(`Server started at ${process.env.PORT}`)
     })
 }
