554e19ca373b52dd243441a3a9bb44f56f06ca17
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-31 16:10:24 +0530

    added city controller

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,91 @@
+const { CityService } = require("../services/index");
+
+const cityService = new CityService();
+
+// POST -> /city
+const create = async (req, res) => {
+  try {
+    const city = await cityService.createCity(req.body);
+    return res.status(201).json({
+      data: city,
+      success: true,
+      message: "Successfully created a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Error creating a city",
+      err: error,
+    });
+  }
+};
+// DELETE -> /city/:id
+const destroy = async (req, res) => {
+  try {
+    const response = await cityService.deleteCity(req.params.id);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: "Successfully deleted a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Error in deleting a city",
+      err: error,
+    });
+  }
+};
+// GET -> /city/:id
+const get = async (req, res) => {
+  try {
+    const response = await cityService.getCity(req.params.id);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: "Successfully retrieved a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Error in retrieving a city",
+      err: error,
+    });
+  }
+};
+// PATCH -> /city/:id -> req.body
+const update = async (req, res) => {
+  try {
+    const response = await cityService.updateCity(req.params.id, req.body);
+    return res.status(200).json({
+        data: response,
+        success: true,
+        message: "Successfully updated a city",
+        err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Error in retrieving a city",
+      err: error,
+    });
+  }
+};
+
+module.exports = {
+  create,
+  destroy,
+  get,
+  update,
+};
