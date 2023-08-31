987878e90828c7947e75f1685563f2c71ea0abae
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-31 16:45:20 +0530

    routers up, api in working state

--- src/index.js
+++ src/index.js
@@ -2,27 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const { PORT } = require('./config/serverConfig')
+const ApiRoutes = require('./routes/index')
 
 const setupAndStartserver = async () => {
     const app = express();
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended:true}))
 
+    app.use('/api', ApiRoutes)
+
     app.listen(3000, async () => {
       console.log(`Server started at ${process.env.PORT}`);
-      /**
-       * DIRECT DB ACCESS CALLS
-       *        Create a new city
-            const { City } = require('./models/index')
-             await City.create({
-                  name: "New Delhi"
-             })
-       * 
-       * FOR REPORITORY CALLS
-            const CityRepository = require('./repository/city-repository')
-            const repo = new CityRepository()
-            await repo.createCity({name: "New Delhi"})
-       */
     })
 }
 setupAndStartserver()
