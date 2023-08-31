10d6448ae78b90d8e293f6a76da440925a3decae
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-31 18:04:32 +0530

    added filters to city api

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,26 +1,27 @@
-const { City } = require('../models/index');
+const { Op } = require("sequelize");
+const { City } = require("../models/index");
 
 class CityRepository {
-    // create city function
+  // create city function
   async createCity({ name }) {
     try {
       const city = await City.create({ name });
       return city;
     } catch (error) {
-      console.log("Something went wrong in the repository layer")
+      console.log("Something went wrong in the repository layer");
       throw { error };
     }
   }
-    // delete cities function
+  // delete cities function
   async deleteCity(cityId) {
     try {
       await City.destroy({ where: { id: cityId } });
     } catch (error) {
       console.log("Something went wrong in the repository layer");
-        throw { error };
+      throw { error };
     }
   }
-    // get all cities function
+  // get all cities function
   async getCity(cityID) {
     try {
       const city = await City.findByPk(cityID);
@@ -30,24 +31,35 @@ class CityRepository {
       throw { error };
     }
   }
-    // update city function
+  // update city function
   async updateCity(cityID, data) {
     // data will be a object like: {name: "Prayagraj"}
-      // const city = await City.update(data, {
-      //   where: {
-      //     id: cityID
-      //   },
-      //   returning: true,
-      //   plain: true
-      // })
-      // the above code will return an array like: [1] in mySQL and will return an object like: {name: "Prayagraj"} in postgres, for similar output we have changed the code to: 
-      const city = await City.findByPk(cityID);
-      city.name = data.name;
-      await city.save();
-      return city;
+    // const city = await City.update(data, {
+    //   where: {
+    //     id: cityID
+    //   },
+    //   returning: true,
+    //   plain: true
+    // })
+    // the above code will return an array like: [1] in mySQL and will return an object like: {name: "Prayagraj"} in postgres, for similar output we have changed the code to:
+    const city = await City.findByPk(cityID);
+    city.name = data.name;
+    await city.save();
+    return city;
   }
-  async getAllCities() {
+  async getAllCities(filter) {
+    // filter can be empty
     try {
+      if (filter.name) {
+        const cities = await City.findAll({
+          where: {
+            name: {
+              [Op.startsWith]: filter.name,
+            }
+          },
+        });
+        return cities;
+      }
       const cities = await City.findAll();
       return cities;
     } catch (error) {
